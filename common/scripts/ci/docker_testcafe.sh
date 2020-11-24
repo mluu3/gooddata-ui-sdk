d14f3311e2ecf76f74db2e0545005d92b7546b9b
Minh Luu <minh.luu@gooddata.com>
2020-11-24 17:18:49 +0700

    abc

--- common/scripts/ci/docker_testcafe.sh
+++ common/scripts/ci/docker_testcafe.sh
@@ -19,7 +19,8 @@ docker run \
     -u $(id -u ${USER}):$(id -g ${USER}) \
     -w /gooddata-ui-sdk \
     ${IMAGE_ID} \
-    chromium -c 5 -q -r spec,xunit:examples/sdk-examples/ci/results/testcafe-results.xml \
+    'chromium --window-size='\''1280,800'\'' --disable-background-networking --disable-background-timer-throttling --disable-backgrounding-occluded-windows --disable-breakpad --disable-client-side-phishing-detection --disable-component-update --disable-default-apps --disable-dev-shm-usage --disable-extensions --disable-features=site-per-process --disable-hang-monitor --disable-infobars --disable-ipc-flooding-protection --disable-popup-blocking --disable-prompt-on-repost --disable-renderer-backgrounding --disable-sync --disable-translate --disable-web-resource --enable-automation --metrics-recording-only --mute-audio --no-first-run --no-sandbox --safebrowsing-disable-auto-update' \
+     -c 2 -q -r spec,xunit:examples/sdk-examples/ci/results/testcafe-results.xml \
     --assertion-timeout 60000 --selector-timeout 10000 \
     --screenshots-on-fails --screenshots examples/sdk-examples/_screenshots \
     --screenshot-path-pattern "${CAPTURE_BASE_PATTERN}.png" \
